9f79bd54e6399e4f382b59833098a968e3649168
cheen <cheen@windows10.microdone.cn>
2018-03-23 16:39:20 +0800

    提交信息

---
+++ src/main/java/com/cheen/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.cheen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/first")
+	public String getFirst() {
+		return "index";
+	}
+}
